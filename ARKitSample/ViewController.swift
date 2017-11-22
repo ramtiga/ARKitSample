424421e99e2347c18da727e2d7e7b436dc9835fa
ramtiga <dhanegm731@gmail.com>
2017-11-23 07:43:05 +0900

    add sample

--- ARKitSample/ViewController.swift
+++ ARKitSample/ViewController.swift
@@ -46,7 +46,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else {
+      return
+    }
+    let result = sceneView.hitTest(touch.location(in: sceneView), types: ARHitTestResult.ResultType.featurePoint)
+    guard let hitResult = result.last else{
+      return
+    }
+    let hitTransform = SCNMatrix4(hitResult.worldTransform)
+    let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+    createBall(position: hitVector)
+  }
+  
+  func createBall(position: SCNVector3) {
+    let ballShape = SCNSphere(radius: 0.01)
+    let ballNode = SCNNode(geometry: ballShape)
+    ballNode.position = position
+    sceneView.scene.rootNode.addChildNode(ballNode)
+  }
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
